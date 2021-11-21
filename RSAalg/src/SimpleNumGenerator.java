f56e407d8bdf9b36511f9fa216d9153d15b03ce2
Sasha Riaboshapka <sashariaboshapka2003@gmail.com>
2021-11-21 21:14:59 +0200

    RSA algo should work now

--- RSAalg/src/SimpleNumGenerator.java
+++ RSAalg/src/SimpleNumGenerator.java
@@ -1,7 +1,20 @@
+import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.security.SecureRandom;
 
 public class SimpleNumGenerator {
     ArrayList <Integer> simpleNumArr=new ArrayList<>();
+
+    public SimpleNumGenerator(){
+        SecureRandom random = new SecureRandom();
+        //Get random number in range 5 - Integer.MAX_VALUE
+        int randomKey = random.nextInt(Integer.MAX_VALUE-4);
+        generatePrimeNumbers(randomKey);
+        //Tests
+        for(int i:simpleNumArr){
+            System.out.print(i+" ");
+        }
+    }
 
     public static boolean isSimple(long x){
         if(x<2)
@@ -15,7 +28,7 @@ public class SimpleNumGenerator {
     }
 
     // Counts randomKey prime numbers and write them in simpleNumArr
-    private void getTwoSimpleNumbers(int randomKey){
+    private void generatePrimeNumbers(int randomKey){
         int cntr=0;
         int currN=3;
         while (cntr<=randomKey){
